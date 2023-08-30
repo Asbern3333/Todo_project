1dd944f4dccd75cab66dd3f7bdceeefef5581b5e
Asbern3333 <asbbernama9@gmail.com>
2023-08-30 18:16:45 -0400

    finish the stater calender

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -3,24 +3,25 @@ import { Outlet ,Link } from "react-router-dom"
 export default function Sidebar() {
 
 return(
-    <div className="h-screen w-screen grid grid-cols-4 bg-blue-500">
+    <div className="h-screen w-screen grid grid-cols-5 bg-blue-500">
      
-       <div className="h-screen w-96 bg-green-400 ">
+       <div className="h-screen w-80 bg-green-400 ">
         <h1 className=" text-center text-white text-5xl text">Pick something</h1> <br />
         <Link to="./schedule" >
-        <button className="h-24 w-96 bg-red-300 hover:bg-blue-400 hover:text-white">schedule</button> </Link>
+        <button className="h-24 w-80 bg-red-300 hover:bg-blue-400 hover:text-white">schedule</button> </Link>
         <br />   <br />
-        <button className="h-24 w-96 bg-red-300 hover:bg-blue-400 hover:text-white">ideas</button> <br /> <br />
-        <button className="h-24 w-96 bg-red-300 hover:bg-blue-400 hover:text-white">Checkdaily tasks</button><br /><br />
-        <button className="h-24 w-96 bg-red-300 hover:bg-blue-400 hover:text-white">SummerY and note</button><br /><br />
-        <button className="h-24 w-96 bg-red-300 hover:bg-blue-400 hover:text-white">Duedate</button> 
-        
+        <Link to="./schedule" ><button className="h-24 w-80 bg-red-300 hover:bg-blue-400 hover:text-white">ideas</button> <br /> <br />
+        </Link> 
+        <Link to="./schedule" >      <button className="h-24 w-80 bg-red-300 hover:bg-blue-400 hover:text-white">Checkdaily tasks</button><br /><br />
+        </Link> 
+        <Link to="./schedule" >  <button className="h-24 w-80 bg-red-300 hover:bg-blue-400 hover:text-white">SummerY and note</button><br /><br />
+        </Link> 
+        <Link to="./schedule" ><button className="h-24 w-80 bg-red-300 hover:bg-blue-400 hover:text-white">Duedate</button> 
+        </Link> 
         </div>
-        <div className="col-span-3">
-        <Outlet  className="flex mb-96 px-96   "/>
+        <div className="col-span-4">
+        <Outlet />
         </div>
-        
-
     </div>
 )
 //  <NavLink
--- Frontend/src/schedule.jsx
+++ Frontend/src/schedule.jsx
@@ -1,37 +1,187 @@
-export default function Schedule(){
-    
+export default function Schedule() {
+
 return (
-        <table className="border-solid border-2 border-indigo-600">
-            <thead >
-                <tr className="border-solid border-2 border-indigo-600">
-                    <th className="border-solid border-2 border-indigo-600">
-                        Sunday
-                    </th >
+    <table className="border-solid border-2 border-indigo-600 mx-3">
+      <thead>
+        <tr className="border-solid border-2 border-indigo-600">
+          <th className="border-solid border-2 border-indigo-600    ">
+            Days/Time
+          </th>
+          <th className="border-solid border-2 border-indigo-600 px-14   ">
+            monday
+          </th>
+          <th className="border-solid border-2 border-indigo-600 px-14">
+            Tuseday
+          </th>
+          <th className="border-solid border-2 border-indigo-600 px-14">
+            Wedensday
+          </th>
+          <th className="border-solid border-2 border-indigo-600 px-14">
+            Thursday
+          </th>
+          <th className="border-solid border-2 border-indigo-600 px-14">
+            Friday
+          </th>
+          <th className="border-solid border-2 border-indigo-600 px-14">
+            Satrday
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            7
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            8
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            9
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            10
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            11 am
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            12 pm
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            1 pm
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            2 pm
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            3 pm
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            4 pm
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            5 pm
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            6 pm
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
 
-                    <th className="border-solid border-2 border-indigo-600">
-                        Sunday
-                    </th>
-
-                    <th className="border-solid border-2 border-indigo-600">
-                        Sunday
-                    </th>
-
-                    <th className="border-solid border-2 border-indigo-600">
-                        Sunday
-                    </th>
-
-                    <th className="border-solid border-2 border-indigo-600">
-                        Sunday
-                    </th>
-                </tr>
-            </thead>
-
-
-
-
-
-        </table>);
-
-
-
-} 
+        <td className="border-solid border-2 border-indigo-600 text-center">
+          8 pm
+        </td>
+        <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        <tr>
+          <td className="border-solid border-2 border-indigo-600 text-center">
+            9 pm
+          </td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+          <td className="border-solid border-2 border-indigo-600 text-left"></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
